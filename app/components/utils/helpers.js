9527fde8e4618714e20d863436bee2f67af9ce66
Christine <baba.christine@gmail.com>
2016-12-08 22:10:36 -0600

    helpers routes app

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -1,3 +1,32 @@
 /**
  * Created by christine on 12/8/16.
  */
+var axios = require('axios');
+var apiCode = 'e2c92c5e5c9840a6893e03a7eb91b85f';
+
+var helpers = {
+    runQuery: function(term, start, end) {
+        var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + term + "&page=0&sort=newest&begin_date=" + start + "0101&end_date=" + end + "0101&api-key=" + apiCode;
+        return axios.get(queryURL)
+            .then(function(response){
+                return response.data.response.docs;
+            });
+    },
+    getSaved: function() {
+        return axios.get('/api/saved')
+            .then(function(response){
+                return response;
+            });
+    },
+    postSaved: function(title, URL, date) {
+        return axios.post('/api/saved', {title:title, URL: URL, date: date}).then(function(result){
+            return result;
+        });
+    },
+    deleteSaved: function(id) {
+        return axios.delete('/api/saved/' + id).then(function(result) {
+            return result;
+        });
+    }
+};
+module.exports = helpers;
